47e7f1aea65a0ed5964ec4cfbabdadfbe3721f60
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-12-17 12:04:59 +0100

    primera prueba node

---
+++ dockerfiles/node/admin/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e 
+
+load_entrypoint_nginx(){
+    bash /root/admin/nginx/admin/start.sh
+}
+
+workdir(){
+    cd /root/admin/node/proyecto/pproyecto
+}
+
+dependencias(){
+    npm install
+    npm run dev -- --host 0.0.0.0 --port 3000
+}
+
+construir_copiar(){
+    npm run build
+    cp -r dist/* /var/www/html/
+}
+config_nginx(){
+    nginx -g 'daemon off;'
+}
+main(){
+    load_entrypoint_nginx
+    workdir
+    dependencias
+    workdir
+    construir_copiar
+    config_nginx
+}
+
+main
